28ec4d4326cf8182ff3129111334877bc0618944
sbutts <sean.butts@scripps.com>
2019-12-03 22:06:03 -0500

    updated layouts and styles

--- app/src/main/java/com/butts/sean/alchemycodingchallenge/App.kt
+++ app/src/main/java/com/butts/sean/alchemycodingchallenge/App.kt
@@ -1,7 +1,18 @@
 package com.butts.sean.alchemycodingchallenge
 
 import android.app.Application
+import com.butts.sean.alchemycodingchallenge.di.injectFeature
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            // declare used Android context
+            androidContext(this@App)
+        }
 
+        injectFeature()
+    }
 }
